c1709565eeebf8cf000f33a512879b4d57dc4197
Adhi <xadhisatrio@gmail.com>
2024-11-23 20:43:40 +0700

    Update query.js

--- query.js
+++ query.js
@@ -1,13 +1,9 @@
-// Retrieve the current data from sessionStorage
 let currentData = JSON.parse(sessionStorage.getItem('__telegram__initParams'));
 
-// If the data exists, modify the tgWebAppData
 if (currentData) {
-  currentData.tgWebAppData = "%TGWEBAPPDATA%";  // Placeholder for tgWebAppData
+  currentData.tgWebAppData = "%TGWEBAPPDATA%";  
   
-  // Store the updated data back in sessionStorage
   sessionStorage.setItem('__telegram__initParams', JSON.stringify(currentData));
 }
 
-// Verify the change
 console.log(sessionStorage.getItem('__telegram__initParams'));
